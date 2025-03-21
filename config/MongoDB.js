294146270c148c8af416bb35180b11557962d999
“TienDung1706“ <tiendung17062k4@gmail.com>
2025-03-21 18:20:54 +0700

    fix deploy 2

--- config/MongoDB.js
+++ config/MongoDB.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 5000, // Giảm timeout xuống 5 giây để bắt lỗi nhanh hơn
         });
         console.log("✅ Kết nối MongoDB thành công!");
