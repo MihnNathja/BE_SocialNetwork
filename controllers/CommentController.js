68553a0f49d5d788963c4e7387649c928715882e
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2025-04-27 21:44:24 +0700

    Xóa log trong hàm getCommentsByPostID

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -126,7 +126,6 @@ exports.getCommentsByPostId = async (req, res) => {
       }
     ]);
     
-    console.log(comments);
     res.status(200).json(comments);
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
